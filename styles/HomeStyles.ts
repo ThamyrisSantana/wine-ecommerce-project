4f0cbae8f35834ec2d85876e94afab620debd853
ThamyrisSantana <thamyrissatana3@gmail.com>
2021-12-30 11:51:21 -0300

    style:WineCardContainer and FilterContainer style

--- styles/HomeStyles.ts
+++ styles/HomeStyles.ts
@@ -7,4 +7,29 @@ export const Container = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: center;
+
+  main {
+    width: 100%;
+    display: flex;
+    justify-content: space-around;
+  }
 `;
+
+export const WineContainer = styled.div`
+  width: 60%;
+  .cards {
+    display: flex;
+    flex-direction: row;
+    justify-content: flex-start;
+    flex-wrap: wrap;
+    gap: 2.5rem;
+
+    flex: 1;
+  }
+`;
+export const FilterContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const c = styled.div``;
