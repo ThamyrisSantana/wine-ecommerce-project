4fbbef1d978f9d1284f506260080bb51689e97dd
ThamyrisSantana <thamyrissatana3@gmail.com>
2022-01-02 11:07:49 -0300

    feat:  adding QueryClientProvider on _app

---
+++ src/pages/_app.tsx
@@ -0,0 +1,15 @@
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { QueryClientProvider, QueryClient } from "react-query";
+
+const queryClient = new QueryClient();
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <QueryClientProvider client={queryClient} contextSharing={true}>
+      <Component {...pageProps} />
+    </QueryClientProvider>
+  );
+}
+
+export default MyApp;
