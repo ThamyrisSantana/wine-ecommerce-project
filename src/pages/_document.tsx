c4262bcb70d2e3180242c0a7deea5e481c51697e
ThamyrisSantana <thamyrissatana3@gmail.com>
2022-01-02 10:40:48 -0300

    feat: document file created and fonts import added

---
+++ src/pages/_document.tsx
@@ -0,0 +1,41 @@
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext,
+} from "next/document";
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx);
+
+    return initialProps;
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link
+            href="http://fonts.cdnfonts.com/css/neo-sans-std"
+            rel="stylesheet"
+          />
+
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700;900&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
