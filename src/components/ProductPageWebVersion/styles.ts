fb80919c5ae774aaee8bfe03e440cea048cf58d2
ThamyrisSantana <thamyrissatana3@gmail.com>
2022-01-03 08:39:59 -0300

    style: ProductPageWebVersion styles added

---
+++ src/components/ProductPageWebVersion/styles.ts
@@ -0,0 +1,170 @@
+import styled from "styled-components";
+
+export const WebVersion = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-self: center;
+  align-items: center;
+  justify-content: center;
+
+  @media (max-width: 918px) {
+    display: none;
+  }
+`;
+
+export const WineContainerWeb = styled.div`
+  width: 50%;
+  height: 100%;
+  display: flex;
+  align-items: flex-start;
+`;
+export const WineInfosWeb = styled.div`
+  width: 35%;
+  background-color: back;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+
+  font-family: "Lato";
+
+  @media (max-width: 1111px) {
+    width: 40%;
+  }
+`;
+
+export const ImageContainerWeb = styled.div`
+  width: 50%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const WineImageWeb = styled.img`
+  width: 50vw;
+  height: 70vh;
+  object-fit: contain;
+`;
+
+export const InfosHeaderWeb = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+`;
+export const WineWeb = styled.span`
+  color: #c81a78;
+  font-weight: 700;
+`;
+
+export const WineCountryWeb = styled.span`
+  color: #c81a78;
+  font-weight: 700;
+`;
+
+export const RegionWeb = styled.span`
+  color: #888888;
+  font-weight: 700;
+`;
+
+export const WineNameWeb = styled.h2`
+  min-height: 2rem;
+  font-family: "Neo Sans Std";
+  font-size: 30px;
+  color: #111111;
+  font-weight: 700;
+  margin: 1.2rem 0 0.8rem 0;
+`;
+
+export const WineFlagWeb = styled.img`
+  width: 18px;
+`;
+
+export const WineAdressWeb = styled.span`
+  width: 100%;
+  display: flex;
+  gap: 0.5rem;
+`;
+
+export const CountryWeb = styled.span`
+  font-size: 14px;
+  color: #555555;
+`;
+export const TypeWeb = styled.span`
+  font-size: 14px;
+  color: #555555;
+`;
+export const ClassificationWeb = styled.span`
+  font-size: 14px;
+  color: #555555;
+`;
+export const SizeWeb = styled.span`
+  font-size: 14px;
+  color: #555555;
+`;
+
+export const StarsContainer = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 0.2rem;
+`;
+
+export const Avaliations = styled.div`
+  margin-left: 1rem;
+`;
+
+export const PriceContainerWeb = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  /* align-items: flex-start; */
+  justify-content: flex-start;
+  margin: 0;
+  margin: 3rem 0;
+`;
+export const PriceMemberContainer = styled.span`
+  color: #c81a78;
+  font-weight: 900;
+`;
+export const RealSign = styled.span`
+  font-size: 30px;
+`;
+export const PriceMember = styled.span`
+  font-size: 45px;
+`;
+export const PriceNonMember = styled.span`
+  font-size: 17px;
+  text-transform: uppercase;
+  font-weight: 700;
+  color: #888888;
+`;
+
+export const DescriptionContainerWeb = styled.div`
+  width: 90%;
+`;
+
+export const DescriptionTitleWeb = styled.span`
+  font-size: 17px;
+  font-weight: 500;
+  color: #111111;
+  font-family: "Neo Sans Std";
+`;
+export const DescriptionParagraphWeb = styled.p`
+  font-size: 15px;
+  font-weight: 400;
+  color: #666666;
+`;
+
+export const Arrow = styled.button`
+  align-self: flex-start;
+  background-color: transparent;
+  border: none;
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+  cursor: pointer;
+`;
+export const BackText = styled.span`
+  font-size: 20px;
+  font-family: "Neo Sans Std";
+`;
