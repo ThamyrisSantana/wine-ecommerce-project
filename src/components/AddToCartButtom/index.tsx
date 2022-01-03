67511fa1fba5ae0e29034e65de005a46ad4a8d71
ThamyrisSantana <thamyrissatana3@gmail.com>
2022-01-03 08:36:04 -0300

    feat: add to cart button component created

---
+++ src/components/AddToCartButtom/index.tsx
@@ -0,0 +1,35 @@
+import { ReactNode, useState } from "react";
+import { HiPlusSm, HiMinusSm } from "react-icons/hi";
+
+import {
+  Container,
+  NumberOfProduct,
+  MinusButton,
+  PlusButton,
+  Number,
+  BuyButton,
+} from "./styles";
+
+function AddToCartButtom(): JSX.Element {
+  const [number, setnumber] = useState(1);
+
+  return (
+    <Container>
+      <NumberOfProduct>
+        <MinusButton
+          onClick={() => (number > 1 ? setnumber(number - 1) : null)}
+          disabled={number <= 1}
+        >
+          <HiMinusSm />
+        </MinusButton>
+        <Number>{number}</Number>
+        <PlusButton onClick={() => setnumber(number + 1)}>
+          <HiPlusSm />
+        </PlusButton>
+      </NumberOfProduct>
+      <BuyButton>Adicionar</BuyButton>
+    </Container>
+  );
+}
+
+export default AddToCartButtom;
