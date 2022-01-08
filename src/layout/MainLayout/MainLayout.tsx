41782bfa8daca16ba8b906fb0c9e3e01f9deb027
ThamyrisSantana <thamyrissatana3@gmail.com>
2022-01-07 21:03:25 -0300

    fix: fixing seach bar

--- src/layout/MainLayout/MainLayout.tsx
+++ src/layout/MainLayout/MainLayout.tsx
@@ -4,15 +4,15 @@ import { Content, Container } from "./styles";
 
 interface Props {
   children: React.ReactNode;
+  search: string;
+  onChangeSearchText: (text: string) => void;
 }
 
-const MainLayout = ({ children }: Props): JSX.Element => {
-  const [search, setSearch] = useState("");
-
-  const onChangeSearchText = (text: string) => {
-    setSearch(text);
-  };
-
+const MainLayout = ({
+  children,
+  onChangeSearchText,
+  search,
+}: Props): JSX.Element => {
   return (
     <Container>
       <Header onChangeSearchText={onChangeSearchText} searchText={search} />
